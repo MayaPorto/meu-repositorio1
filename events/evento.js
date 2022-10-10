14802905f5328376b28d3a3b6fb4645ceca987f1
MayaPorto <mayamiresporto@gmail.com>
2022-10-10 14:14:39 -0300

    eventosDom

--- events/evento.js
+++ events/evento.js
@@ -7,3 +7,8 @@ function fecharAlert() {
     const divAlerta = document.getElementById('alerta')
     divAlerta.classList.remove('mostrar')
 }
+
+const btnMostrar = document.getElementById('mostrar')
+const btnFechar = document.getElementById('fechar')
+
+btnMostrar.addEventListener('click', mostrarAlerta)
